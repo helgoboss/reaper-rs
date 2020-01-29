c27ac5553d7eaf0434e448a3e44733f5899765e3
Benjamin Klum <benjamin.klum@helgoboss.org>
2020-01-29 01:34:03 +0100

    Delegate all control surface calls

--- main/src/high_level/helper_control_surface.rs
+++ main/src/high_level/helper_control_surface.rs
@@ -1,10 +1,92 @@
 use crate::medium_level::ControlSurface;
+use std::os::raw::c_void;
+use crate::low_level::MediaTrack;
 
 pub struct HelperControlSurface {
 }
 
 impl ControlSurface for HelperControlSurface {
-    fn run(&self) {
-        println!("Moin from HelperControlSurface!!!")
+    fn GetTypeString(&self) -> *const i8 {
+        unimplemented!()
+    }
+
+    fn GetDescString(&self) -> *const i8 {
+        unimplemented!()
+    }
+
+    fn GetConfigString(&self) -> *const i8 {
+        unimplemented!()
+    }
+
+    fn CloseNoReset(&self) {
+        unimplemented!()
+    }
+
+    fn Run(&self) {
+        unimplemented!()
+    }
+
+    fn SetTrackListChange(&self) {
+        unimplemented!()
+    }
+
+    fn SetSurfaceVolume(&self, trackid: *mut MediaTrack, volume: f64) {
+        unimplemented!()
+    }
+
+    fn SetSurfacePan(&self, trackid: *mut MediaTrack, pan: f64) {
+        unimplemented!()
+    }
+
+    fn SetSurfaceMute(&self, trackid: *mut MediaTrack, mute: bool) {
+        unimplemented!()
+    }
+
+    fn SetSurfaceSelected(&self, trackid: *mut MediaTrack, selected: bool) {
+        unimplemented!()
+    }
+
+    fn SetSurfaceSolo(&self, trackid: *mut MediaTrack, solo: bool) {
+        unimplemented!()
+    }
+
+    fn SetSurfaceRecArm(&self, trackid: *mut MediaTrack, recarm: bool) {
+        unimplemented!()
+    }
+
+    fn SetPlayState(&self, play: bool, pause: bool, rec: bool) {
+        unimplemented!()
+    }
+
+    fn SetRepeatState(&self, rep: bool) {
+        unimplemented!()
+    }
+
+    fn SetTrackTitle(&self, trackid: *mut MediaTrack, title: *const i8) {
+        unimplemented!()
+    }
+
+    fn GetTouchState(&self, trackid: *mut MediaTrack, isPan: i32) -> bool {
+        unimplemented!()
+    }
+
+    fn SetAutoMode(&self, mode: i32) {
+        unimplemented!()
+    }
+
+    fn ResetCachedVolPanStates(&self) {
+        unimplemented!()
+    }
+
+    fn OnTrackSelection(&self, trackid: *mut MediaTrack) {
+        unimplemented!()
+    }
+
+    fn IsKeyDown(&self, key: i32) -> bool {
+        unimplemented!()
+    }
+
+    fn Extended(&self, call: i32, parm1: *mut c_void, parm2: *mut c_void, parm3: *mut c_void) -> i32 {
+        unimplemented!()
     }
 }
--- main/src/medium_level/control_surface.rs
+++ main/src/medium_level/control_surface.rs
@@ -1,73 +1,150 @@
-use crate::low_level::MediaTrack;
 use std::ptr::null_mut;
+
+use crate::low_level::MediaTrack;
 
 // TODO This should probably be moved to low_level API!
 pub trait ControlSurface {
-    fn run(&self);
+    fn GetTypeString(&self) -> *const ::std::os::raw::c_char;
 
+    fn GetDescString(&self) -> *const ::std::os::raw::c_char;
+
+    fn GetConfigString(&self) -> *const ::std::os::raw::c_char;
+
+    fn CloseNoReset(&self);
+
+    fn Run(&self);
+
+    fn SetTrackListChange(&self);
+
+    fn SetSurfaceVolume(&self, trackid: *mut MediaTrack, volume: f64);
+
+    fn SetSurfacePan(&self, trackid: *mut MediaTrack, pan: f64);
+
+    fn SetSurfaceMute(&self, trackid: *mut MediaTrack, mute: bool);
+
+    fn SetSurfaceSelected(&self, trackid: *mut MediaTrack, selected: bool);
+
+    fn SetSurfaceSolo(&self, trackid: *mut MediaTrack, solo: bool);
+
+    fn SetSurfaceRecArm(&self, trackid: *mut MediaTrack, recarm: bool);
+
+    fn SetPlayState(&self, play: bool, pause: bool, rec: bool);
+
+    fn SetRepeatState(&self, rep: bool);
+
+    fn SetTrackTitle(
+        &self,
+        trackid: *mut MediaTrack,
+        title: *const ::std::os::raw::c_char,
+    );
+
+    fn GetTouchState(
+        &self,
+        trackid: *mut MediaTrack,
+        isPan: ::std::os::raw::c_int,
+    ) -> bool;
+
+    fn SetAutoMode(&self, mode: ::std::os::raw::c_int);
+
+    fn ResetCachedVolPanStates(&self);
+
+    fn OnTrackSelection(&self, trackid: *mut MediaTrack);
+
+    fn IsKeyDown(&self, key: ::std::os::raw::c_int) -> bool;
+
+    fn Extended(
+        &self,
+        call: ::std::os::raw::c_int,
+        parm1: *mut ::std::os::raw::c_void,
+        parm2: *mut ::std::os::raw::c_void,
+        parm3: *mut ::std::os::raw::c_void,
+    ) -> ::std::os::raw::c_int;
 }
 
 #[no_mangle]
 extern "C" fn GetTypeString(callback_target: *mut Box<dyn ControlSurface>) -> *const ::std::os::raw::c_char {
-    null_mut()
+    let control_surface = unsafe { Box::from_raw(callback_target) };
+    control_surface.GetTypeString()
 }
 
 #[no_mangle]
 extern "C" fn GetDescString(callback_target: *mut Box<dyn ControlSurface>) -> *const ::std::os::raw::c_char {
-    null_mut()
+    let control_surface = unsafe { Box::from_raw(callback_target) };
+    control_surface.GetDescString()
 }
 
 #[no_mangle]
 extern "C" fn GetConfigString(callback_target: *mut Box<dyn ControlSurface>) -> *const ::std::os::raw::c_char {
-    null_mut()
+    let control_surface = unsafe { Box::from_raw(callback_target) };
+    control_surface.GetConfigString()
 }
 
 #[no_mangle]
 extern "C" fn CloseNoReset(callback_target: *mut Box<dyn ControlSurface>) {
+    let control_surface = unsafe { Box::from_raw(callback_target) };
+    control_surface.CloseNoReset()
 }
 
 #[no_mangle]
 extern "C" fn Run(callback_target: *mut Box<dyn ControlSurface>) {
     // "Decode" thin pointer
     // (see https://users.rust-lang.org/t/sending-a-boxed-trait-over-ffi/21708/6)
-    let control_surface = unsafe { Box::from_raw(callback_target)};
-    control_surface.run();
+    let control_surface = unsafe { Box::from_raw(callback_target) };
+    control_surface.Run()
 }
 
 #[no_mangle]
 extern "C" fn SetTrackListChange(callback_target: *mut Box<dyn ControlSurface>) {
+    let control_surface = unsafe { Box::from_raw(callback_target) };
+    control_surface.SetTrackListChange()
 }
 
 #[no_mangle]
 extern "C" fn SetSurfaceVolume(callback_target: *mut Box<dyn ControlSurface>, trackid: *mut MediaTrack, volume: f64) {
+    let control_surface = unsafe { Box::from_raw(callback_target) };
+    control_surface.SetSurfaceVolume(trackid, volume)
 }
 
 #[no_mangle]
 extern "C" fn SetSurfacePan(callback_target: *mut Box<dyn ControlSurface>, trackid: *mut MediaTrack, pan: f64) {
+    let control_surface = unsafe { Box::from_raw(callback_target) };
+    control_surface.SetSurfacePan(trackid, pan)
 }
 
 #[no_mangle]
 extern "C" fn SetSurfaceMute(callback_target: *mut Box<dyn ControlSurface>, trackid: *mut MediaTrack, mute: bool) {
+    let control_surface = unsafe { Box::from_raw(callback_target) };
+    control_surface.SetSurfaceMute(trackid, mute)
 }
 
 #[no_mangle]
 extern "C" fn SetSurfaceSelected(callback_target: *mut Box<dyn ControlSurface>, trackid: *mut MediaTrack, selected: bool) {
+    let control_surface = unsafe { Box::from_raw(callback_target) };
+    control_surface.SetSurfaceSelected(trackid, selected)
 }
 
 #[no_mangle]
 extern "C" fn SetSurfaceSolo(callback_target: *mut Box<dyn ControlSurface>, trackid: *mut MediaTrack, solo: bool) {
+    let control_surface = unsafe { Box::from_raw(callback_target) };
+    control_surface.SetSurfaceSolo(trackid, solo)
 }
 
 #[no_mangle]
 extern "C" fn SetSurfaceRecArm(callback_target: *mut Box<dyn ControlSurface>, trackid: *mut MediaTrack, recarm: bool) {
+    let control_surface = unsafe { Box::from_raw(callback_target) };
+    control_surface.SetSurfaceRecArm(trackid, recarm)
 }
 
 #[no_mangle]
 extern "C" fn SetPlayState(callback_target: *mut Box<dyn ControlSurface>, play: bool, pause: bool, rec: bool) {
+    let control_surface = unsafe { Box::from_raw(callback_target) };
+    control_surface.SetPlayState(play, pause, rec)
 }
 
 #[no_mangle]
 extern "C" fn SetRepeatState(callback_target: *mut Box<dyn ControlSurface>, rep: bool) {
+    let control_surface = unsafe { Box::from_raw(callback_target) };
+    control_surface.SetRepeatState(rep)
 }
 
 #[no_mangle]
@@ -76,6 +153,9 @@ extern "C" fn SetTrackTitle(
     trackid: *mut MediaTrack,
     title: *const ::std::os::raw::c_char,
 ) {
+
+    let control_surface = unsafe { Box::from_raw(callback_target) };
+    control_surface.SetTrackTitle(trackid, title)
 }
 
 #[no_mangle]
@@ -84,24 +164,32 @@ extern "C" fn GetTouchState(
     trackid: *mut MediaTrack,
     isPan: ::std::os::raw::c_int,
 ) -> bool {
-    false
+    let control_surface = unsafe { Box::from_raw(callback_target) };
+    control_surface.GetTouchState(trackid, isPan)
 }
 
 #[no_mangle]
 extern "C" fn SetAutoMode(callback_target: *mut Box<dyn ControlSurface>, mode: ::std::os::raw::c_int) {
+    let control_surface = unsafe { Box::from_raw(callback_target) };
+    control_surface.SetAutoMode(mode)
 }
 
 #[no_mangle]
 extern "C" fn ResetCachedVolPanStates(callback_target: *mut Box<dyn ControlSurface>) {
+    let control_surface = unsafe { Box::from_raw(callback_target) };
+    control_surface.ResetCachedVolPanStates()
 }
 
 #[no_mangle]
 extern "C" fn OnTrackSelection(callback_target: *mut Box<dyn ControlSurface>, trackid: *mut MediaTrack) {
+    let control_surface = unsafe { Box::from_raw(callback_target) };
+    control_surface.OnTrackSelection(trackid)
 }
 
 #[no_mangle]
 extern "C" fn IsKeyDown(callback_target: *mut Box<dyn ControlSurface>, key: ::std::os::raw::c_int) -> bool {
-    false
+    let control_surface = unsafe { Box::from_raw(callback_target) };
+    control_surface.IsKeyDown(key)
 }
 
 #[no_mangle]
@@ -112,5 +200,6 @@ extern "C" fn Extended(
     parm2: *mut ::std::os::raw::c_void,
     parm3: *mut ::std::os::raw::c_void,
 ) -> ::std::os::raw::c_int {
-    0
+    let control_surface = unsafe { Box::from_raw(callback_target) };
+    control_surface.Extended(call, parm1, parm2, parm3)
 }
