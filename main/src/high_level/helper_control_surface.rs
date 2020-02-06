5b8f876b2442a59eda4b3c2c4e81db58faeb310c
Benjamin Klum <benjamin.klum@helgoboss.org>
2020-02-07 00:53:33 +0100

    Implement pan and vol change subjects

--- main/src/high_level/helper_control_surface.rs
+++ main/src/high_level/helper_control_surface.rs
@@ -3,14 +3,16 @@ use crate::low_level::{MediaTrack, ReaProject};
 use crate::medium_level::ControlSurface;
 use std::ffi::CStr;
 use std::borrow::Cow;
-use crate::high_level::{Reaper, Project, Task, Track};
+use crate::high_level::{Reaper, Project, Task, Track, LightTrack};
 use rxrust::prelude::*;
-use std::cell::{RefCell, Cell};
+use std::cell::{RefCell, Cell, RefMut, Ref};
 use std::sync::mpsc::Receiver;
 use crate::high_level::guid::Guid;
 use std::collections::{HashSet, HashMap};
 use c_str_macro::c_str;
 use std::ptr::null_mut;
+use std::collections::hash_map::Entry;
+use std::collections::hash_map::Entry::Occupied;
 
 const BULK_TASK_EXECUTION_COUNT: usize = 100;
 
@@ -19,7 +21,7 @@ pub struct HelperControlSurface {
     last_active_project: Cell<Project>,
     num_track_set_changes_left_to_be_propagated: Cell<i32>,
     fx_has_been_touched_just_a_moment_ago: Cell<bool>,
-    track_data_by_media_track_by_rea_project: RefCell<HashMap<*mut ReaProject, TrackDataMap>>,
+    track_datas: RefCell<ProjectTrackDataMap>,
     fx_chain_pair_by_media_track: RefCell<HashMap<*mut MediaTrack, FxChainPair>>,
 
     // Capabilities depending on REAPER version
@@ -51,6 +53,7 @@ struct FxChainPair {
     output_fx_guids: HashSet<Guid>,
 }
 
+type ProjectTrackDataMap = HashMap<*mut ReaProject, TrackDataMap>;
 type TrackDataMap = HashMap<*mut MediaTrack, TrackData>;
 
 impl HelperControlSurface {
@@ -62,7 +65,7 @@ impl HelperControlSurface {
             last_active_project: Cell::new(reaper.get_current_project()),
             num_track_set_changes_left_to_be_propagated: Default::default(),
             fx_has_been_touched_just_a_moment_ago: Default::default(),
-            track_data_by_media_track_by_rea_project: Default::default(),
+            track_datas: Default::default(),
             fx_chain_pair_by_media_track: Default::default(),
             // since pre1,
             supports_detection_of_input_fx: version >= c_str!("5.95").into(),
@@ -85,8 +88,27 @@ impl HelperControlSurface {
         }
     }
 
-    fn find_track_data_by_track<'a>(&self, track: *mut MediaTrack) -> Option<&'a mut TrackData> {
-        unimplemented!()
+    fn find_track_data_in_normal_state<'a>(&self, track: *mut MediaTrack) -> Option<RefMut<TrackData>> {
+        if self.get_state() == State::PropagatingTrackSetChanges {
+            return None;
+        }
+        self.find_track_data(track)
+    }
+
+    fn find_track_data_map(&self) -> Option<RefMut<TrackDataMap>> {
+        let rea_project = Reaper::instance().get_current_project().get_rea_project();
+        if (!self.track_datas.borrow().contains_key(&rea_project)) {
+            return None;
+        }
+        Some(RefMut::map(self.track_datas.borrow_mut(), |tds| tds.get_mut(&rea_project).unwrap()))
+    }
+
+    fn find_track_data<'a>(&self, track: *mut MediaTrack) -> Option<RefMut<TrackData>> {
+        let track_data_map = self.find_track_data_map()?;
+        if (!track_data_map.contains_key(&track)) {
+            return None;
+        }
+        Some(RefMut::map(track_data_map, |tdm| tdm.get_mut(&track).unwrap()))
     }
 
     fn track_parameter_is_automated(&self, track: *mut MediaTrack, parameter_name: &CStr) -> bool {
@@ -95,26 +117,6 @@ impl HelperControlSurface {
 }
 
 impl ControlSurface for HelperControlSurface {
-    fn set_surface_pan(&self, trackid: *mut MediaTrack, pan: f64) {
-        if self.get_state() == State::PropagatingTrackSetChanges {
-            return;
-        }
-        let td = match self.find_track_data_by_track(trackid) {
-            None => return,
-            Some(td) => td
-        };
-        if td.pan == pan {
-            return;
-        }
-        td.pan = pan;
-        let track = Track::new(trackid, null_mut());
-        // trackPanChangedSubject_.get_subscriber().on_next(track);
-        if !self.track_parameter_is_automated(trackid, c_str!("Pan")) {
-            // trackPanTouchedSubject_.get_subscriber().on_next(track);
-        }
-    }
-
-
     fn run(&mut self) {
         for task in self.task_receiver.try_iter().take(BULK_TASK_EXECUTION_COUNT) {
             task();
@@ -127,6 +129,41 @@ impl ControlSurface for HelperControlSurface {
         if (new_active_project != self.last_active_project.get()) {
             self.last_active_project.replace(new_active_project);
             reaper.subjects.project_switched.borrow_mut().next(new_active_project);
+        }
+    }
+
+    fn set_surface_pan(&self, trackid: *mut MediaTrack, pan: f64) {
+        let mut td = match self.find_track_data_in_normal_state(trackid) {
+            None => return,
+            Some(td) => td
+        };
+        if td.pan == pan {
+            return;
+        }
+        td.pan = pan;
+        let track = LightTrack::new(trackid, null_mut());
+        let reaper = Reaper::instance();
+        reaper.subjects.track_pan_changed.borrow_mut().next(track);
+        if !self.track_parameter_is_automated(trackid, c_str!("Pan")) {
+            reaper.subjects.track_pan_touched.borrow_mut().next(track);
+        }
+    }
+
+
+    fn set_surface_volume(&self, trackid: *mut MediaTrack, volume: f64) {
+        let mut td = match self.find_track_data_in_normal_state(trackid) {
+            None => return,
+            Some(td) => td
+        };
+        if td.volume == volume {
+            return;
+        }
+        td.volume = volume;
+        let track = LightTrack::new(trackid, null_mut());
+        let reaper = Reaper::instance();
+        reaper.subjects.track_volume_changed.borrow_mut().next(track);
+        if !self.track_parameter_is_automated(trackid, c_str!("Volume")) {
+            reaper.subjects.track_volume_touched.borrow_mut().next(track);
         }
     }
 }
