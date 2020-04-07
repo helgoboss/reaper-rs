9ad35f642c8b7f1bef19dc551497d2c2bf5391b5
Benjamin Klum <benjamin.klum@helgoboss.org>
2020-04-07 23:10:55 +0200

    Make bindgen optional

--- main/build.rs
+++ main/build.rs
@@ -1,6 +1,7 @@
 use cc;
 
 fn main() {
+    #[cfg(feature = "generate-bindings")]
     generate_bindings();
     compile_glue();
 }
@@ -13,17 +14,10 @@ fn compile_glue() {
         .compile("glue");
 }
 
+#[cfg(feature = "generate-bindings")]
 fn generate_bindings() {
-    // Tell cargo to invalidate the built crate whenever the wrapper changes
     println!("cargo:rerun-if-changed=src/low_level/bindgen.hpp");
-
-    // The bindgen::Builder is the main entry point
-    // to bindgen, and lets you build up options for
-    // the resulting bindings.
-
     let bindings = bindgen::Builder::default()
-        // The input header we would like to generate
-        // bindings for.
         .header("src/low_level/bindgen.hpp")
         .opaque_type("timex")
         .derive_eq(true)
@@ -151,9 +145,7 @@ fn generate_bindings() {
         // Tell cargo to invalidate the built crate whenever any of the
         // included header files changed.
         .parse_callbacks(Box::new(bindgen::CargoCallbacks))
-        // Finish the builder and generate the bindings.
         .generate()
-        // Unwrap the Result and panic on failure.
         .expect("Unable to generate bindings");
     // Write the bindings to the bindings.rs file.
     let out_path = std::path::PathBuf::from(std::env::var("CARGO_MANIFEST_DIR").unwrap());
