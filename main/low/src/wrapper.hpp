2d6c72d458f62132932f0e9f890a3596488658b0
Benjamin Klum <benjamin.klum@helgoboss.org>
2020-05-14 22:50:52 +0200

    Document wrapper.hpp in great detail

--- main/low/src/wrapper.hpp
+++ main/low/src/wrapper.hpp
@@ -1,15 +1,21 @@
-// This file aggregates all header files that serve as input for generating `bindings.rs`.
+// This file aggregates all header files that serve as input for generating `bindings.rs` with bindgen.
 
-// Make all types relevant for REAPER plug-in visible
+// Make all types relevant for REAPER plug-ins visible to bindgen.
 #include "../lib/reaper/reaper_plugin.h"
 
-// Make all REAPER functions visible under dedicated namespace
+// Make all REAPER functions (function pointers to be exact) visible under dedicated namespace, so we can easily include
+// them with bindgen. None of the function pointers will actually be used in the real plug-in. They just serve as
+// input for stage two of the generation process: The generation of `reaper.rs`.
 namespace reaper_functions {
   #include "../lib/reaper/reaper_plugin_functions.h"
   #include "../lib/reaper/more_reaper_plugin_functions.h"
 }
 
-// Make all SWELL functions visible under dedicated namespace
+// Make all SWELL functions visible under dedicated namespace, so we can easily include them with bindgen.
+// Above inclusion of "reaper_plugin.h" already included SWELL as well. So we need to undefine some stuff to be able
+// to include it again - under a different namespace and with SWELL_PROVIDED_BY_APP defined, which makes the SWELL
+// functions end up as function pointers - exactly like the REAPER functions. They also serve as input for stage two
+// of the generation process: The generation of `swell.rs`.
 #undef _WDL_SWELL_H_API_DEFINED_
 #undef SWELL_API_DEFINE
 #define SWELL_PROVIDED_BY_APP
@@ -17,6 +23,6 @@ namespace swell_functions {
 #include "../lib/WDL/WDL/swell/swell-functions.h"
 }
 
-// C++ glue code function signatures
+// Make C++ glue code functions visible to bindgen. They will be used in the real application.
 #include "control_surface.hpp"
 #include "midi.hpp"
